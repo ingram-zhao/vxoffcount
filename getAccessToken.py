4bf929b5a3ab2dd32650f7a833bca128605eb8c1
Ingramzhao <1154283293@qq.com>
2020-07-19 21:26:49 +0800

    add read logpath

--- getAccessToken.py
+++ getAccessToken.py
@@ -1,9 +1,18 @@
 # encoding: utf-8
 
 import time
+import os
 import logging
 import configparser
 import requests
+
+# 读取配置
+config = configparser.ConfigParser()
+config.read("./config.ini")
+log = config.get('default','log')
+appid = config.get('auth','appid')
+appsecret = config.get('auth','appsecret')
+
 
 # 定义日志
 logger = logging.getLogger('test')
@@ -11,15 +20,11 @@ logger.setLevel(level=logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
-file_handler = logging.FileHandler('./logs/getAccessToken.log')
+file_handler = logging.FileHandler(log)
 file_handler.setLevel(level=logging.INFO)
 file_handler.setFormatter(formatter)
 
-# 读取配置
-config = configparser.ConfigParser()
-config.read("./config.ini")
-appid = config.get('auth','appid')
-appsecret = config.get('auth','appsecret')
+
 
 def getNewAccessToken():
     url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s" % (appid,appsecret)
@@ -29,6 +34,7 @@ def getNewAccessToken():
         logger.error('errcode: %s , errmsg: %s' % (responsedata['errcode'],responsedata['errmsg']))
         return False
     access_token = responsedata["access_token"]
+
     with open("./token",mode='w',encoding='utf8') as f:
         f.write(access_token)
     logger.info('getNewAcessToken Success！') 

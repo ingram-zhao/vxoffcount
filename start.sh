465b7c8fc6f208636762699b859ae0dc463a59e1
Ingramzhao <1154283293@qq.com>
2020-07-19 22:58:26 +0800

    update step3

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
 
 # 1.安装第三方模块：requirements.txt
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt > /dev/null
 
 # 2.启动定时任务：getAccessToken.py
 nohup python3 getAccessToken.py > ./logs/getAccessToken.out &
 
 # 3.生成自定义菜单：vxapi.py
-python3 vxapi.py 2> vxapi.out
+python3 vxapi.py > /dev/null
 
 # 4.启动服务：main.py 80
 nohup python3 main.py 80 > ./logs/start.out &

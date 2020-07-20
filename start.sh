06ec84551522d0498366d18666746ebf7672a93a
Ingramzhao <1154283293@qq.com>
2020-07-20 17:09:28 +0800

    update start.sh file

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 pip3 install -r requirements.txt > /dev/null
 
 # 2.启动定时任务：getAccessToken.py
-nohup python3 getAccessToken.py > ./logs/getAccessToken.out &
+nohup python3 -u getAccessToken.py > ./logs/getAccessToken.out 2>&1 &
 
 # 3.生成自定义菜单：vxapi.py
 python3 vxapi.py > /dev/null
 
 # 4.启动服务：main.py 80
-nohup python3 main.py 80 > ./logs/start.out &
+nohup python3 -u main.py 80 > ./logs/start.out 2>&1 &
 
 
